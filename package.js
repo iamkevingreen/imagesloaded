324fd1239af76518465cd777fe034eb254b03663
Kevin Green <kevin@swedenunlimited.com>
2015-01-07 14:36:21 -0500

    git init

---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  name: "iamkevingreen:imagesloaded",
+  git: "https://github.com/iamkevingreen/imagesloaded.git",
+  summary: "Meteor packaging of imagesloaded",
+  "version": "1.0.0"
+});
+Package.onUse(function(api) {
+
+  api.versionsFrom('1.0.2.1');
+  api.use('jquery', 'client');
+
+  api.addFiles('imagesloaded/imagesloaded.pkgd.min.js');
+});
