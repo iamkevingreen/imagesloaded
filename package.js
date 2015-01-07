414d161769df32f6e5dba43aeb30e65d5ced8f25
Kevin Green <kevin@swedenunlimited.com>
2015-01-07 14:59:55 -0500

    updated the versions

--- package.js
+++ package.js
@@ -2,12 +2,12 @@ Package.describe({
   name: "iamkevingreen:imagesloaded",
   git: "https://github.com/iamkevingreen/imagesloaded.git",
   summary: "Meteor packaging of imagesloaded",
-  "version": "1.0.0"
+  "version": "1.0.1"
 });
 Package.onUse(function(api) {
 
   api.versionsFrom('1.0.2.1');
   api.use('jquery', 'client');
 
-  api.addFiles('imagesloaded/imagesloaded.pkgd.min.js');
+  api.addFiles('imagesloaded/imagesloaded.js');
 });
